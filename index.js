0995b0a0928777666bfb4772bb4060a624f495d7
Justin Tweed <tweedjustin@gmail.com>
2026-06-03 09:40:41 -0400

    Started re-ordering js and doing separation of concerns by separating js files

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-const budget = document.body.querySelector('#budget_number');
-const budgetUsed = document.body.querySelector('#budget_used');
-const budgetLeft = document.body.querySelector('#budget_left');
-const barMarker = document.querySelector('#marker_bar');
-const barFill = document.querySelector('.bar-fill');
-const paymentButton = document.querySelector('#payment_button');
-const paymentPopup = document.querySelector('#payment_popup');
-const closeButton = document.querySelector('.close-btn');
+//these are the imports from other js documents.
+import {budget, budgetUsed, budgetLeft, barMarker, barFill, paymentButton, paymentPopup, closeButton} from './variables.js';
 
 //the class below tracks the total budget of the month.
+//need to add logic to the setters to make sure the inputs are not going below 0
 class Budget {
     constructor(subscriptions, phone, internet){
         this._rent = 1450;
@@ -89,6 +84,8 @@ class Budget {
 }
 
 //the class below tracks what payments the user did.
+//Need to make an array as one property and have the user input them into the constructor arguements.
+//then loop through it as a method instead of individually adding them
 class Payments {
     constructor(rent, carPayments, grocery, electric, subscriptions, phone, internet, miscellaneous){
         this._rent = rent;
@@ -179,41 +176,67 @@ class Payments {
     }
 }
 
+//this is logic colors checking the percentage and coloring the bar differently.
+function colorBar(){
+    if(usedPercent < 50){
+        barFill.style.background = '#4caf50';
+    } else if(usedPercent < 90){
+        barFill.style.background = '#ffeb3b';
+    } else {
+        barFill.style.background = '#f44336';
+    }
+}
+
+//made a budget using the Budget Class.
 const justinBudget = new Budget(60, 45, 105);
 
+//made a payment using the Payment Class.
 const justinPayment = new Payments(0, 744, 600, 250, 60, 45, 105, 0);
 
+//method that calculates the total budget.
 const totalBudget = justinBudget.budgetTotal();
+
+//method that calculates the total spent.
 const spent = justinPayment.paymentTotal();
+
+//simple subtraction between totalBudget and spent to get difference into a new variable.
 const remainingBudget = totalBudget - spent;
+
+//using Math.min and Math.max putting a clamp on 100% being the cap and 0% being the lowest. Using this for bar.
 const usedPercent = Math.min(100, Math.max(0, (spent / totalBudget) * 100));
 
-
+//fills bar 
 barFill.style.width = `${usedPercent}%`;
+
+//adds a marker at the end of the bar.
 barMarker.style.left = `${usedPercent}%`;
 
+colorBar();
 
-
+//Budget Total
 budget.innerHTML = totalBudget;
+
+//Budget Used
 budgetUsed.innerHTML = spent;
+
+//Budget Left
 budgetLeft.innerHTML = remainingBudget;
+
+//defaults popup to be hidden in beginning of script.
 paymentPopup.classList.add('hidden');
 
+//this is for the Payments button to allow the popup to be seen.
 paymentButton.addEventListener('click', () => {
     paymentPopup.classList.remove('hidden');
 })
 
+//this is the close or x button on the popup to close the window.
 closeButton.addEventListener('click', () => {
     paymentPopup.classList.add('hidden');
 })
 
-if(usedPercent < 50){
-    barFill.style.background = '#4caf50';
-} else if(usedPercent < 90){
-    barFill.style.background = '#ffeb3b';
-} else {
-    barFill.style.background = '#f44336';
-}
+
+
 
 
 
---
+++ variables.js
@@ -0,0 +1,8 @@
+export const budget = document.body.querySelector('#budget_number');
+export const budgetUsed = document.body.querySelector('#budget_used');
+export const budgetLeft = document.body.querySelector('#budget_left');
+export const barMarker = document.querySelector('#marker_bar');
+export const barFill = document.querySelector('.bar-fill');
+export const paymentButton = document.querySelector('#payment_button');
+export const paymentPopup = document.querySelector('#payment_popup');
+export const closeButton = document.querySelector('.close-btn');
